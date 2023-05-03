44e584d0a17ad41018ff72e2af7ac013c7954421
Joao Paulo Vieira <vieira.joaopaulo@hotmail.com>
2023-05-03 17:50:54 -0300

    Add support to IncrCounterWithAttrs metrics

--- metrics.go
+++ metrics.go
@@ -5,4 +5,6 @@ type Metrics interface {
 	IncrCounter(name string)
 	// PushToSeries adds a new value to a histogram identified by the given name.
 	PushToSeries(name string, value float64)
+	// IncrCounterWithAttrs increments the counter value identified by the given name while adding attributes.
+	IncrCounterWithAttrs(name string, attributes map[string]string)
 }
